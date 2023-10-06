f1bec2d89e026d0f1a3ffd1ad31c51c9d2d02d53
Equipo <Equipo@LAPTOP-DOU>
2023-10-05 18:59:23 -0600

    Practica 2 inicio

---
+++ src/main/java/com/voleyinformation/VoleyinformationApplication.java
@@ -0,0 +1,13 @@
+package com.voleyinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoleyinformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoleyinformationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/voleyinformation/VoleyinformationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voleyinformation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoleyinformationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
